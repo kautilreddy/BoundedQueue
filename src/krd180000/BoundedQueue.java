00047c9a1e3392386eda1d6e7666c4d9e959f02a
kautilreddy <KAU14@MacBook-Pro.local>
2018-09-05 21:07:59 -0500

    added documentations, test cases and code

--- src/krd180000/BoundedQueue.java
+++ src/krd180000/BoundedQueue.java
@@ -6,53 +6,70 @@ package krd180000;
  */
 public class BoundedQueue<T> {
     private Object[] data;
-    private int size;
-    private int head;
-    private int tail;
+    private int size; //no of elements currently in the queue
+    private int capacity; //max size current queue can store
+    private int head; //index of earliest inserted entry
+    private int tail; //index of the next free place
+
+    /**
+     *  Initializes a BoundedQueue of given size
+     * @param size Queue capacity
+     * @throws IllegalArgumentException when given size is zero or negative
+     */
     public BoundedQueue(int size){
-        this.size=0;
-        this.head=-1;
-        this.tail=-1;
+        if(size<=0){
+            throw new IllegalArgumentException("incorrect size value given for queue");
+        }
+        clear();
+        this.capacity = size;
         data = new Object[size];
     }
 
     /**
      * add a new element x at the rear of the queue
      * returns false if the element was not added because the queue is full
-     * @param x
-     * @return
+     * @param x element to be added
+     * @return boolean representing if element is added to queue or not
      */
     public boolean offer(T x){
+        if(size>=capacity)
+            return false;
         size++;
-        tail = (tail+1)/size;
+        data[tail] = x;
+        tail = (tail+1)%capacity;
         return true;
     }
 
     /**
      * remove and return the element at the front of the queue
      * return null if the queue is empty
-     * @return
+     * @return the element removed from queue or null if empty
      */
     public T poll(){
+        if(size==0)
+            return null;
         T returnVal = cast(data[head]);
+        head = (head+1)%capacity;
         size--;
         return returnVal;
     }
 
     /**
-     * return the number of elements in the queue
-     * @return
+     * return front element, without removing it (null if queue is empty)
+     * @return front element or null (if empty)
      */
     public T peek(){
+        if(size==0)
+            return null;
         return cast(data[head]);
     }
 
     /**
      * return the number of elements in the queue
-     * @return
+     * @return number of elements currently in the queue
      */
     public int size(){
-        return data.length;
+        return size;
     }
 
     /**
@@ -68,6 +85,8 @@ public class BoundedQueue<T> {
      */
     public void clear(){
         size=0;
+        head=0;
+        tail=0;
     }
 
     /**
@@ -75,7 +94,14 @@ public class BoundedQueue<T> {
      * @param a array to be filled
      */
     public void toArray(T[] a){
-
+        if(a.length<size){
+            throw new IllegalArgumentException("size of passed array is too small");
+        }
+        int cursor = head;
+        for(int i=0;i<size;++i){
+            a[i] = cast(data[cursor]);
+            cursor=(cursor+1)%capacity;
+        }
     }
 
     /**
--- src/krd180000/TestMain.java
+++ src/krd180000/TestMain.java
@@ -1,5 +1,7 @@
 package krd180000;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -38,76 +40,136 @@ public class TestMain {
     }
 
     private void testOffer() {
+        BoundedQueue<Integer> queue = new BoundedQueue<>(3);
         //offer on empty queue
-
+        assert queue.offer(1);
         //offer on normal queue
-
+        assert queue.offer(2);
         //offer on one less than full
-
+        assert queue.offer(3);
         //offer on full
+        assert !queue.offer(4);
+        //offer when tail is at 0
+        assert queue.poll()==1;
+        assert queue.offer(4);
     }
 
     private void testPoll() {
+        BoundedQueue<Integer> queue = new BoundedQueue<>(3);
         //poll on empty queue
-
+        assert queue.poll()==null;
         //poll on one more than empty
-
+        queue.offer(1);
+        assert queue.poll()==1;
         //poll on normal queue
-
+        queue.offer(1);
+        queue.offer(2);
         //poll on one less than full
-
+        assert queue.poll()==1;
+        queue.offer(3);
+        queue.offer(4);
         //poll on full
-
+        assert queue.poll() == 2;
+        assert queue.poll() == 3;
+        assert queue.poll() == 4;
+        //poll when head and tail are same but not at 0
+        assert queue.poll() == null;
     }
 
     private void testPeek() {
+        BoundedQueue<Integer> queue = new BoundedQueue<>(3);
         //peek on empty
-
+        assert queue.peek()==null;
         //peek on one more than empty
-
-        //peek on normal queue
+        queue.offer(1);
+        assert queue.peek()==1;
+        assert queue.size()==1;
 
         //peek on one less than full
+        queue.offer(2);
+        assert queue.peek() == 1;
 
         //peek on full
+        queue.offer(3);
+        assert queue.peek() == 1;
+        assert queue.size()==3;
+        queue.poll();
+        assert queue.peek() == 2;
     }
 
     private void testSize() {
-
+        BoundedQueue<Integer> queue = new BoundedQueue<>(3);
+        assert queue.size()==0;
+        queue.offer(1);
+        assert queue.size()==1;
+        queue.offer(2);
+        assert queue.size()==2;
+        queue.offer(3);
+        assert queue.size()==3;
+        queue.poll();
+        queue.offer(4);
+        assert queue.size()==3;
     }
 
     private void testIsEmpty() {
-        //when full
-
+        BoundedQueue<Integer> queue = new BoundedQueue<>(3);
         //when empty
+        assert queue.isEmpty();
+        //when full
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        assert !queue.isEmpty();
+        //when empty but head and tail are not at initial position
+        queue.poll();
+        queue.poll();
+        queue.poll();
+        assert queue.isEmpty();
     }
 
     private void testClear() {
+        BoundedQueue<Integer> queue = new BoundedQueue<>(3);
         //clear on empty
-
+        queue.clear();
+        assert queue.isEmpty();
         //clear on normal
-
-        //clear on full
+        queue.offer(1);
+        queue.offer(2);
+        queue.clear();
+        assert queue.isEmpty();
     }
 
     private void testToArray() {
+        BoundedQueue<Integer> queue = new BoundedQueue<>(3);
+        Integer arr[] = new Integer[3];
         //on empty
-
+        queue.toArray(arr);
+        assert arr[0]==null;
+        assert arr[1]==null;
+        assert arr[2]==null;
         //on normal
+        queue.offer(1);
+        queue.offer(2);
+        queue.toArray(arr);
+        assert arr[0]==1;
+        assert arr[1]==2;
+        assert arr[2]==null;
 
         //on full
-    }
+        queue.offer(3);
+        queue.poll();
+        queue.offer(4);
+        queue.toArray(arr);
+        assert arr[0]==2;
+        assert arr[1]==3;
+        assert arr[2]==4;
 
-    /**
-     * Runs the given function inside a try-catch block to capture and assert that NoSuchElementException is raised
-     * @param function function that needs to be run inside the try block
-     */
-    private void checkNoSuchElementException(Runnable function){
+        //check exception when small array is passed
         try {
-            function.run();
+            queue.toArray(new Integer[1]);
             assert false;
         }catch (Exception e){
-            assert e instanceof NoSuchElementException;
+            assert e instanceof IllegalArgumentException;
         }
     }
 }
